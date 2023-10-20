32c1346d69c73b0ac2b12313b662c35a15ee64ad
Surya K N <95220715+Surya-KN@users.noreply.github.com>
2023-10-21 02:29:41 +0530

    crawl.js and tests added

--- crawl.js
+++ crawl.js
@@ -1,13 +1,42 @@
+const { JSDOM } = require('jsdom')
+
+function getURLfromHTML(htmlBody,baseURL) {
+	const url = []
+	const dom = new JSDOM(htmlBody)
+	const linkele = dom.window.document.querySelectorAll('a')
+	// console.log(linkele.)
+	for(const l of linkele){
+		if(l.href.slice(0,1) === '/'){
+			try{
+			const urlObj = new URL(`${baseURL}${l.href}`)
+			url.push(urlObj.href)
+			}catch(err){
+				console.log("invalid")
+			}
+		}else{
+			try{
+			const urlObj = new URL(l.href)
+			url.push(urlObj.href)
+			}catch(err){
+					console.log("invalid")
+			}
+		}
+	}
+	return url
+}
+
+
 function normalizeUrl(urlString){
 	const urlObject = new URL(urlString)
 	const hostpath = `${urlObject.hostname}${urlObject.pathname}`
 	// hostpath = hostpath.toLowerCase
-	console.log(hostpath.toLocaleLowerCase)
+	// console.log(hostpath.toLocaleLowerCase)
 	if(hostpath.slice(-1) === '/') return hostpath.slice(0,-1)
 	return hostpath
 }
 
 
 module.exports = {  
-  normalizeUrl
+  normalizeUrl,
+  getURLfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeUrl } = require('./crawl.js')
+const { normalizeUrl,getURLfromHTML} = require('./crawl.js')
 const {test,expect} = require('@jest/globals')
 
 test('normalizeUrl strip protocol',() =>{
@@ -26,6 +26,63 @@ test('normalizeUrl strip http',() =>{
   const input = 'http://BLOG.boot.dev/path/'
   const actual = normalizeUrl(input)
   const excepted = 'blog.boot.dev/path'
+  expect(actual).toEqual(excepted)
+})
+
+test('getURLfromHTML absoloute',() =>{
+  const input = `
+  <html>
+    <body>
+      <a href = "https://blog.boot.dev/path"></a>
+    </body>
+  </html>
+  `
+  const baseURL = 'https://blog.boot.dev/path'
+  const actual = getURLfromHTML(input,baseURL)
+  const excepted = ['https://blog.boot.dev/path']
+  expect(actual).toEqual(excepted)
+})
+
+test('getURLfromHTML relative',() =>{
+  const input = `
+  <html>
+    <body>
+      <a href = "https://blog.boot.dev/path"></a>
+      <a href = "/path"></a>
+    </body>
+  </html>
+  `
+  const baseURL = 'https://blog.boot.dev'
+  const actual = getURLfromHTML(input,baseURL)
+  const excepted = ['https://blog.boot.dev/path','https://blog.boot.dev/path']
+  expect(actual).toEqual(excepted)
+})
+
+test('getURLfromHTML both',() =>{
+  const input = `
+  <html>
+    <body>
+      <a href = "/path"></a>
+    </body>
+  </html>
+  `
+  const baseURL = 'https://blog.boot.dev'
+  const actual = getURLfromHTML(input,baseURL)
+  const excepted = ['https://blog.boot.dev/path']
+  expect(actual).toEqual(excepted)
+})
+
+test('getURLfromHTML invalid',() =>{
+  const input = `
+  <html>
+    <body>
+      <a href = "path"></a>
+    </body>
+  </html>
+  `
+  const baseURL = 'https://blog.boot.dev'
+  const actual = getURLfromHTML(input,baseURL)
+  const excepted = []
   expect(actual).toEqual(excepted)
 })
 
